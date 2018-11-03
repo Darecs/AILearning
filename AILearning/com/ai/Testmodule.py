ee165321869440c1e8fc9a55ae956f36ba1a6b41
Darecs <j.kuehn91@googlemail.com>
2018-11-03 21:35:12 +0100

    First Commit

---
+++ AILearning/com/ai/Testmodule.py
@@ -0,0 +1,26 @@
+'''
+Created on 03.11.2018
+
+@author: juliankuhn
+'''
+
+class TestClass():
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+    
+    def helloWorld(self, testSentence):
+        print(testSentence)
+        
+        
+
+a = TestClass()
+a.helloWorld("Hello World")
+
